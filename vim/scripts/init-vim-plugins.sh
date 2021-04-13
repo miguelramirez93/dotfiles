9bebd3d004dcd9176d6e8906e58c61f72c88fe01
miguelramirez93 <darckzios@gmail.com>
2021-04-13 10:23:25 -0500

    Added go config

--- vim/scripts/init-vim-plugins.sh
+++ vim/scripts/init-vim-plugins.sh
@@ -9,4 +9,5 @@ install_vim_package ../plugins/fugitive ~/.vim/pack/tpope/start
 install_vim_package ../plugins/vim-airline ~/.vim/pack/dist/start
 install_vim_package ../plugins/vim-gitgutter ~/.vim/pack/airblade/start
 install_vim_package ../plugins/typescript-vim ~/.vim/pack/typescript/start
+install_vim_package ../plugins/vim-go ~/.vim/pack/plugins/start
 
