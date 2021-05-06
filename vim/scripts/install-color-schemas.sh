114a0d9e4aab017b9bc3947d6bd95a838785d41f
miguelramirez93 <darckzios@gmail.com>
2021-05-06 09:33:06 -0500

    Add dracula theme & remove line break config

--- vim/scripts/install-color-schemas.sh
+++ vim/scripts/install-color-schemas.sh
@@ -2,4 +2,4 @@
 source ./utils.sh
 echo "installing color schemas"
 install_vim_package ../color-schemes/onedark.vim ~/.vim/pack/*/opt
-
+install_vim_package ../color-schemes/dracula ~/.vim/pack/*/opt
