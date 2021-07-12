96e23cbce92ecf3d19b898c6cf1d8a982a363f21
miguelramirez93 <darckzios@gmail.com>
2021-07-12 08:43:21 -0500

    Added xcode themes

--- vim/scripts/install-color-schemas.sh
+++ vim/scripts/install-color-schemas.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source ./utils.sh
 echo "installing color schemas"
-install_vim_package ../color-schemes/onedark.vim ~/.vim/pack/*/opt
 install_vim_package ../color-schemes/dracula ~/.vim/pack/*/opt
+install_vim_package ../color-schemes/vim-colors-xcode ~/.vim/pack/*/opt
