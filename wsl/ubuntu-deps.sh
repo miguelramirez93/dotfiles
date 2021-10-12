48f9c026f3292ec50062b7bb5cdbdd9afe7e5db2
miguelramirez93 <darckzios@gmail.com>
2021-10-12 14:51:33 -0500

    Added nvim unstable version

--- wsl/ubuntu-deps.sh
+++ wsl/ubuntu-deps.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+sudo apt-get update -y
 sudo apt-get install ripgrep -y
 sudo apt-get install fzf -y
 sudo apt install fd-find -y
+
+sudo add-apt-repository ppa:neovim-ppa/unstable -y
+sudo apt-get update -y
+sudo apt-get install neovim -y
