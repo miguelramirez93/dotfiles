ba056397cfa4ea8d26fb1be1e3b8062cd70991f4
miguelramirezml93 <jose.ramirez@mercadolibre.com.co>
2022-05-24 13:44:16 -0500

    WIP: Added inline warnnings for lsp and theme manager

--- vim/lua/user/colorscheme.lua
+++ vim/lua/user/colorscheme.lua
@@ -1,8 +1,10 @@
 vim.g.material_style = "palenight"
 
+require("themer").setup({})
+
 vim.cmd [[
 try
-  colorscheme material
+  colorscheme themer_onedark
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
   set background=dark
