af13e0690e7d745d7d8d9cb868036bd4118a0f9f
miguelramirezml93 <jose.ramirez@mercadolibre.com.co>
2023-12-05 22:09:32 -0500

    Add git helper

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -39,4 +39,7 @@ return {
     "nvim-telescope/telescope-file-browser.nvim",
     dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
   },
+  {
+    "tpope/vim-fugitive",
+  },
 }
