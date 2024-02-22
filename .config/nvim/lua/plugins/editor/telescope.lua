f1a0d325fdd4fa00c30cf0407b9cebbb0d759a06
miguelramirezml93 <jose.ramirez@mercadolibre.com.co>
2024-02-22 07:28:13 -0500

    Add zsh and kitty cfgs

--- .config/nvim/lua/plugins/editor/telescope.lua
+++ .config/nvim/lua/plugins/editor/telescope.lua
@@ -4,14 +4,14 @@ return {
 	version = false, -- telescope did only one release, so use HEAD for now
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		-- {
-		-- 	"nvim-telescope/telescope-fzf-native.nvim",
-		-- 	build = "make",
-		-- 	enabled = vim.fn.executable("make") == 1,
-		-- 	config = function()
-		-- 		require("telescope").load_extension("fzf")
-		-- 	end,
-		-- },
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+			enabled = vim.fn.executable("make") == 1,
+			config = function()
+				require("telescope").load_extension("fzf")
+			end,
+		},
 		-- {
 		-- 	"nvim-telescope/telescope-file-browser.nvim",
 		-- 	config = function()
