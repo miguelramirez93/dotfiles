d24f32a62140288872f72d74d26e296d1c5f97c8
joser <jose.ramire@mercadolibre.com.co>
2024-06-07 14:56:09 -0500

    Add better colorscheme cfg

--- .config/nvim/lua/specs/plugins/ui/colorscheme.lua
+++ .config/nvim/lua/specs/plugins/ui/colorscheme.lua
@@ -10,15 +10,39 @@
 --         vim.cmd.colorscheme("gruvbox-material")
 --     end,
 -- }
-local src = "folke/tokyonight.nvim"
+local themes_cfg = {
+	tokyonight = {
+		req_path = "tokyonight",
+		colorscheme_name = "tokyonight-moon",
+		src = "folke/tokyonight.nvim",
+		opts = {
+			transparent = true,
+		},
+	},
+	catppuccin = {
+		req_path = "catppuccin",
+		name = "catppuccin",
+		colorscheme_name = "catppuccin-mocha",
+		src = "catppuccin/nvim",
+		opts = {
+			transparent_background = true,
+		},
+	},
+}
+
+local selected = "catppuccin"
+
 return {
-	src,
+	themes_cfg[selected].src,
+	name = themes_cfg[selected].name or nil,
 	priority = 1000,
 	lazy = false,
-	setup = function()
-		require("tokyonight").setup({
-			transparent = true,
-		})
-		vim.cmd.colorscheme("tokyonight-moon")
+	opts = themes_cfg[selected].opts,
+	setup = function(opts)
+		if themes_cfg[selected].setup then
+			themes_cfg[selected].setup(opts)
+		end
+		require(themes_cfg[selected].req_path).setup(opts)
+		vim.cmd.colorscheme(themes_cfg[selected].colorscheme_name)
 	end,
 }
