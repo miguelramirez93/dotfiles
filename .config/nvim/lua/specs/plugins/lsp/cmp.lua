1dbd6208d229569a30e77d3be82aaa82e4b4f549
joser <jose.ramire@mercadolibre.com.co>
2024-09-10 13:38:52 -0500

    Change signature provider

--- .config/nvim/lua/specs/plugins/lsp/cmp.lua
+++ .config/nvim/lua/specs/plugins/lsp/cmp.lua
@@ -7,7 +7,7 @@ return {
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-nvim-lsp-signature-help",
+		-- "hrsh7th/cmp-nvim-lsp-signature-help",
 		{
 			"saadparwaiz1/cmp_luasnip",
 			dependencies = {
@@ -38,7 +38,7 @@ return {
 			},
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
-				{ name = "nvim_lsp_signature_help" },
+				-- { name = "nvim_lsp_signature_help" },
 				{ name = "luasnip" },
 				{ name = "path" },
 			}, {
