d722af1ab0715b0c3d3092ff753770619e6f78d9
miguelramirez93 <darckzios@gmail.com>
2024-09-18 14:25:21 -0500

    Add oil current file

--- .config/nvim/lua/specs/plugins/ui/oil.lua
+++ .config/nvim/lua/specs/plugins/ui/oil.lua
@@ -6,6 +6,9 @@ return {
 		view_options = {
 			show_hidden = true,
 		},
+		win_options = {
+			winbar = "%{v:lua.require('oil').get_current_dir()}",
+		},
 	},
 	-- Optional dependencies
 	-- dependencies = { { "echasnovski/mini.icons", opts = {} } },
