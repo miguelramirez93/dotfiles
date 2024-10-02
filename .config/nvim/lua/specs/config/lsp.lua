a608fea1748733b61a0c49a8bd24a759236e3bf9
miguelramirez93 <darckzios@gmail.com>
2024-10-01 22:07:53 -0500

    Add better quickfix navigation

--- .config/nvim/lua/specs/config/lsp.lua
+++ .config/nvim/lua/specs/config/lsp.lua
@@ -24,3 +24,18 @@ vim.diagnostic.config({
 		},
 	},
 })
+
+-- close quickfix list after select an item
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "qf" },
+	command = [[nnoremap <buffer> <CR> <CR>:cclose<CR>]],
+})
+
+-- auto select quickfix element
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "qf",
+	callback = function()
+		vim.keymap.set("n", "<Up>", "<Up><CR><C-w>p", { buffer = true, remap = false, desc = "Navigate up quickfix" })
+		vim.keymap.set("n", "<Down>", "<Down><CR><C-w>p", { remap = false, desc = "Navigate down quickfix" })
+	end,
+})
