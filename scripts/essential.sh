10fab22ed9144424b526b57ea54bc31f50c1d879
miguelramirez93 <darckzios@gmail.com>
2025-07-30 15:13:10 -0500

    Add essential scripts

---
+++ scripts/essential.sh
@@ -0,0 +1,5 @@
+brew install cmake mercurial fzf viu chafa luarocks ripgrep rust go
+sudo apt install git nitrogen zsh rofi
+
+ref rofi https://github.com/newmanls/rofi-themes-collection 
+    https://github.com/jluttine/rofi-power-menu
